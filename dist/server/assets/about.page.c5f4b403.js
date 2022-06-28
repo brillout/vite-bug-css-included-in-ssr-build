f081f1848e1730d9a5e1c644f411b2b212161ece
Romuald Brillout <git@brillout.com>
2022-06-28 20:56:34 +0200

    add dist/ to repo

---
+++ dist/server/assets/about.page.c5f4b403.js
@@ -0,0 +1,15 @@
+import "react";
+import { j as jsxs, F as Fragment, a as jsx } from "./chunk-20fcc0cc.js";
+const index = /* @__PURE__ */ (() => "h1,\np {\n  color: green;\n}\n")();
+function Page() {
+  return /* @__PURE__ */ jsxs(Fragment, {
+    children: [/* @__PURE__ */ jsx("h1", {
+      children: "About"
+    }), /* @__PURE__ */ jsx("p", {
+      children: "A colored page."
+    })]
+  });
+}
+export {
+  Page
+};
