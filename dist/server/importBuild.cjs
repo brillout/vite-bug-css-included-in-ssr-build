bce6f6ca01901908ddf44d7ce1986552b164639f
Romuald Brillout <git@brillout.com>
2022-06-29 18:51:50 +0200

    remove dist/

--- dist/server/importBuild.cjs
+++
@@ -1,6 +0,0 @@
-const { setDistEntries } = require('../../node_modules/.pnpm/vite-plugin-ssr@0.4.0-beta.42_vite@2.9.13/node_modules/vite-plugin-ssr/dist/cjs/node/plugin/plugins/distEntries/loadDistEntries.js');
-setDistEntries({
-  pageFiles: () => import('./pageFiles.js'),
-  clientManifest: () => require('../client/manifest.json'),
-  pluginManifest: () => require('../client/vite-plugin-ssr.json'),
-});
